d8968683a7fa8b97aaed302d2d5ea4fe72f67f09
Darenn Keller <keller.darenn@gmail.com>
2017-01-20 23:21:59 +0100

    Make a first part for movement.

--- Thii Fuze/Assets/Scripts/PlayerController.cs
+++ Thii Fuze/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    public float Speed;
+
+    Rigidbody2D rb;
+
+    Vector2 velocity;
+
+    private void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void LateUpdate () {
+        Vector2 move;
+        rb.velocity -= velocity;
+        move.x = Input.GetAxis("HorizontalJ1") + Input.GetAxis("HorizontalJ2");
+        move.y = Input.GetAxis("VerticalJ1") + Input.GetAxis("VerticalJ2");
+        velocity = move * Speed * Time.deltaTime;
+        rb.AddForce(move * Speed * Time.deltaTime/2);
+        rb.velocity += velocity;
 	}
 }
