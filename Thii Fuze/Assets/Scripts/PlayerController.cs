9bf2060b96bd151396a21fbdcd7b6a2d586df263
Althemar <damien.crechet@gmail.com>
2017-01-20 23:21:19 +0100

    A trace is now draw behind the player

--- Thii Fuze/Assets/Scripts/PlayerController.cs
+++ Thii Fuze/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    private float _speed;
+    private Trace _testLine;
+
 	// Use this for initialization
 	void Start () {
-		
+        _speed = 2f;
+        _testLine = GetComponent<Trace>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        bool arrowPressed = false;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * _speed * Time.deltaTime;
+            arrowPressed = true;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * _speed * Time.deltaTime;
+            arrowPressed = true;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * _speed * Time.deltaTime;
+            arrowPressed = true;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * _speed * Time.deltaTime;
+            arrowPressed = true;
+        }
+
+        if (arrowPressed)
+            _testLine.addPoint(transform.position);
+    }
 }
---
+++ Thii Fuze/Assets/Scripts/Trace.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Trace : MonoBehaviour
+{
+    private List<Vector3> _points;
+    private LineRenderer _lineRenderer;
+    private float _minDistanceBetweenPoints;
+
+    private void Start()
+    {
+        _points = new List<Vector3>();
+        _lineRenderer = GetComponent<LineRenderer>();
+        _minDistanceBetweenPoints = 0.1f;
+    }
+
+    private void Update()
+    {
+        _lineRenderer.numPositions = _points.Count;
+        for (int i = 0; i < _points.Count; i++)
+            _lineRenderer.SetPosition(i, _points[i]);
+    }
+
+    public void addPoint(Vector3 newPos)
+    {
+        if (_points.Count == 0)
+            _points.Add(newPos);
+        else
+        {
+            float distanceFromLast = Vector3.Distance(newPos, _points[_points.Count - 1]);
+            if (distanceFromLast >= _minDistanceBetweenPoints)
+                _points.Add(newPos);
+        }
+    }
+}
