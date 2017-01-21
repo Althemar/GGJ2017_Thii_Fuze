6d2134edb10611b1938af997fee4f0e45c148eb3
Althemar <damien.crechet@gmail.com>
2017-01-21 13:27:34 +0100

    Des LineRenderers sont créés quand boucle

---
+++ Thii Fuze/Assets/Scripts/Edge.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge
+{
+
+    private int _id;
+    private Node _node1;
+    private Node _node2;
+
+    public Edge(Node node1, Node node2, int id)
+    {
+        _id = id;
+        _node1 = node1;
+        _node2 = node2;
+    }
+
+    /*
+     * Getters
+     */
+
+    public int getId()
+    {
+        return _id;
+    }
+
+    public Node getNode1()
+    {
+        return _node1;
+    }
+
+    public Node getNode2()
+    {
+        return _node2;
+    }
+
+}
