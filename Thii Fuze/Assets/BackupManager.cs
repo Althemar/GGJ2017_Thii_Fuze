373d45c02d888ca65dedf027de33b3a3e3cda576
Darenn Keller <keller.darenn@gmail.com>
2017-01-22 14:44:11 +0100

    Added backup scene.

---
+++ Thii Fuze/Assets/BackupManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BackupManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameController.OnGameOver += reloadSceneDelayed;
+        GameController.OnGameWon += reloadSceneDelayed;
+    }
+
+    void reloadSceneDelayed()
+    {
+        StartCoroutine(reloadScene());
+    }
+
+    IEnumerator reloadScene()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void OnDestroy()
+    {
+        GameController.OnGameOver -= reloadSceneDelayed;
+        GameController.OnGameWon -= reloadSceneDelayed;
+    }
+}
